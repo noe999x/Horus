2f721c5bb659362a3df1a9d9a11f0a690ed536e4
ikz <68614343+noe999x@users.noreply.github.com>
2023-02-02 16:31:46 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,12 @@
+import os, time
+from os import system as sis
+from time import sleep as turu
+from os import mkdir as dor
+try:sis("git pull")
+except:pass
+try:dor("RESULTS")
+except:pass
+try:dor("DATA")
+except:pass
+import main
+#(:)kontol.
